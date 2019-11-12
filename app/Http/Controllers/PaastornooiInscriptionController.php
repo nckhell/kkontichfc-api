7b80daadd421e1d9e9b56b12b18a99be6ee221a8
Nick <NickHellemans93@gmail.com>
2019-11-12 19:01:51 +0100

    feat(paastornooi):confirmation email to contactperson

--- app/Http/Controllers/PaastornooiInscriptionController.php
+++ app/Http/Controllers/PaastornooiInscriptionController.php
@@ -25,11 +25,15 @@ class PaastornooiInscriptionController extends Controller
 
         $paastornooiInscription = PaastornooiInscription::create($request->all());
 
-		// Send e-mail
         Mail::send('mails.paastornooi-confirmation', ['data' => $request->all()], function ($m) {
             $m->from('no-reply@kkontichfc.be', 'K. Kontich F.C.');
             $m->to('jeugd@kkontichfc.be', 'Jeugd - K. Kontich F.C.')->subject('Inschrijving Paastornooi via kkontichfc.be');
             $m->bcc('nickhellemans93+kkfc@gmail.com', 'Nick Hellemans');
+        });
+
+        Mail::send('mails.paastornooi-confirmation-client', ['data' => $request->all()], function ($m) {
+            $m->from('no-reply@kkontichfc.be', 'K. Kontich F.C.');
+            $m->to($paastornooiInscription->contactEmail, $paastornooiInscription->contactName)->subject('Inschrijving Paastornooi via kkontichfc.be');
         });
 
         return (new PaastornooiInscriptionResource($paastornooiInscription))
